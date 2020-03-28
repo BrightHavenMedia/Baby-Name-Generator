d31863530b5710aa29ab75344753ab78a3e8246f
jamesej <jensen.jedward@gmail.com>
2020-03-28 14:11:32 -0600

    Picker works and is fully functioning. Still need to add all years to the picker. Currently it works and it will scroll through years and dismiss with the done or a tap out of the UIPickerView.

--- babyNameGenerator/StartScreen.swift
+++ babyNameGenerator/StartScreen.swift
@@ -34,6 +34,12 @@ class StartScreen: UIViewController, UIPickerViewDelegate, UIPickerViewDataSourc
         dateFieldPicker.inputView = datePicker
         datePicker.delegate = self
         
+        let tapGestureRecognizer: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(StartScreen.dismissKeyboard))
+        
+        view.addGestureRecognizer(tapGestureRecognizer)
+        
+        addDoneButtonOnKeyboard()
+        
         
         
         boyButton.layer.cornerRadius = 20
@@ -42,6 +48,11 @@ class StartScreen: UIViewController, UIPickerViewDelegate, UIPickerViewDataSourc
         girlButton.clipsToBounds = true
         navigationItem.title = "Baby Name Generator"
         
+    }
+    
+    @objc func dismissKeyboard() {
+        
+        view.endEditing(true)
     }
     
     
@@ -55,10 +66,31 @@ class StartScreen: UIViewController, UIPickerViewDelegate, UIPickerViewDataSourc
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         dateFieldPicker.text = listOfYears[row]
+        dateFieldPicker.textColor = UIColor.black
     }
     
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         return listOfYears[row]
+    }
+    
+    func addDoneButtonOnKeyboard(){
+        let doneToolbar: UIToolbar = UIToolbar(frame: CGRect.init(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 50))
+        doneToolbar.barStyle = .default
+        
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let done: UIBarButtonItem = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(doneButtonAction))
+        
+        let items = [flexSpace, done]
+        doneToolbar.items = items
+        doneToolbar.sizeToFit()
+        
+        dateFieldPicker.inputAccessoryView = doneToolbar
+        
+    }
+    
+    @objc func doneButtonAction(){
+        dateFieldPicker.resignFirstResponder()
+    
     }
     
 }
